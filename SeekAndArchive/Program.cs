33080cb8ed45c2ad73cbd5758ea43c5bd4d3a400
bozsob <bozso.beatrix@gmail.com>
2017-06-28 15:18:49 +0200

    file searching in given directory - recursively

--- SeekAndArchive/Program.cs
+++ SeekAndArchive/Program.cs
@@ -3,91 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace SeekAndArchive
 {
     class Program
     {
-        static System.Collections.Specialized.StringCollection log =
-            new System.Collections.Specialized.StringCollection();
+        static List<FileInfo> foundFiles;
+
+        static void RecursiveSearch
+            (List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            // enumerate all the files in the current directory
+            foreach (FileInfo file in currentDirectory.GetFiles())
+            {
+                if (file.Name == fileName)
+                {
+                    foundFiles.Add(file);
+                }
+            }
+            // continue the search recursively
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
+        }
 
         static void Main(string[] args)
         {
-            // Start with drives if you have to search the entire computer.
-            string[] drives = System.Environment.GetLogicalDrives();
+            string fileName = args[0];
+            string directoryName = args[1];
 
-            foreach (string dr in drives)
+            foundFiles = new List<FileInfo>();
+
+            // check if the directory exists
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+
+            if (!rootDir.Exists)
             {
-                System.IO.DriveInfo di = new System.IO.DriveInfo(dr);
-
-                // Here we skip the drive if it is not ready to be read. This
-                // is not necessarily the appropriate action in all scenarios.
-                if (!di.IsReady)
-                {
-                    Console.WriteLine("The drive {0} could not be read", di.Name);
-                    continue;
-                }
-                System.IO.DirectoryInfo rootDir = di.RootDirectory;
-                WalkDirectoryTree(rootDir);
+                Console.WriteLine("The specified directory does not exist.");
+                return;
             }
+            // search recursively for the mathing files
+            RecursiveSearch(foundFiles, fileName, rootDir);
 
-            // Write out all the files that could not be processed.
-            Console.WriteLine("Files with restricted access:");
-            foreach (string s in log)
+            // list the found files
+            Console.WriteLine("Found {0} files", foundFiles.Count);
+            foreach(FileInfo file in foundFiles)
             {
-                Console.WriteLine(s);
+                Console.WriteLine("{0}", file.FullName);
             }
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key");
             Console.ReadKey();
-        }
-
-        static void WalkDirectoryTree(System.IO.DirectoryInfo root)
-        {
-            System.IO.FileInfo[] files = null;
-            System.IO.DirectoryInfo[] subDirs = null;
-
-            // First, process all the files directly under this folder
-            try
-            {
-                files = root.GetFiles("*.*");
-            }
-            // This is thrown if even one of the files requires permissions greater
-            // than the application provides.
-            catch (UnauthorizedAccessException e)
-            {
-                // This code just writes out the message and continues to recurse.
-                // You may decide to do something different here. For example, you
-                // can try to elevate your privileges and access the file again.
-                log.Add(e.Message);
-            }
-
-            catch (System.IO.DirectoryNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            if (files != null)
-            {
-                foreach (System.IO.FileInfo fi in files)
-                {
-                    // In this example, we only access the existing FileInfo object. If we
-                    // want to open, delete or modify the file, then
-                    // a try-catch block is required here to handle the case
-                    // where the file has been deleted since the call to TraverseTree().
-                    Console.WriteLine(fi.FullName);
-                }
-
-                // Now find all the subdirectories under this directory.
-                subDirs = root.GetDirectories();
-
-                foreach (System.IO.DirectoryInfo dirInfo in subDirs)
-                {
-                    // Resursive call for each subdirectory.
-                    WalkDirectoryTree(dirInfo);
-                }
-            }
-
         }
     }
 }
